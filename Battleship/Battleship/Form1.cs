76076d30e6acf32bc2dfd90a475001b006a53621
R4F42 <45702449+R4F42@users.noreply.github.com>
2019-02-21 22:15:39 -0600

    Added horizontal ships and found several bugs

--- Battleship/Battleship/Form1.cs
+++ Battleship/Battleship/Form1.cs
@@ -19,8 +19,16 @@ namespace Battleship
                                {0, 0, 0, 0, 0, 0, 0, 0},
                                {0, 0, 0, 0, 0, 0, 0, 0},
                                {0, 0, 0, 0, 0, 0, 0, 0},
-                               {0, 0, 0, 0, 0, 0, 0, 0}, };
-        
+                               {0, 0, 0, 0, 0, 0, 0, 0}};
+
+        public int[,] attackgrid = { {0, 0, 0, 0, 0, 0, 0, 0},
+                                     {0, 0, 0, 0, 0, 0, 0, 0},
+                                     {0, 0, 0, 0, 0, 0, 0, 0},
+                                     {0, 0, 0, 0, 0, 0, 0, 0},
+                                     {0, 0, 0, 0, 0, 0, 0, 0},
+                                     {0, 0, 0, 0, 0, 0, 0, 0},
+                                     {0, 0, 0, 0, 0, 0, 0, 0},
+                                     {0, 0, 0, 0, 0, 0, 0, 0}};
 
         public int[] shippos = new int[4];
         public int accu = 1;
@@ -43,7 +51,20 @@ namespace Battleship
                 }
             }
         }
-        
+        private void GetAttackGrid(int[,] gridname)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    lblAttack.Text += gridname[i, j].ToString() + "      ";
+                    if (j == 7)
+                    {
+                        lblAttack.Text += "\r\n\r\n";
+                    }
+                }
+            }
+        }
         private void ResetGrid()
         {
             int[,] grid =     {{ 0, 0, 0, 0, 0, 0, 0, 0},
@@ -60,7 +81,7 @@ namespace Battleship
             int x1 = shippos[0], y1 = shippos[1], x2 = shippos[2], y2 = shippos[3];
             int xdif = 0, ydif = 0;
 
-            if(x1 - x2 < 0) //Find diff between x's
+            if(x1 - x2 < 0) // Find diff between x's
             {
                 xdif = (x1 - x2) * -1;
             }
@@ -69,7 +90,7 @@ namespace Battleship
                 xdif = x1 - x2;
             }
 
-            if (y1 - y2 < 0) //Find diff between y's
+            if (y1 - y2 < 0) // Find diff between y's
             {
                 ydif = (y1 - y2) * -1;
             }
@@ -78,24 +99,17 @@ namespace Battleship
                 ydif = y1 - y2;
             }
             int n1 = 0;
-            if (y1 == y2) //If you are staying in the column
+            if (y1 == y2) // If you are staying in the column
             {
                 if (xdif == 3 && accu == 1)
                 {
                     for(int i = x1; i <= x2; i++)
                     {
-                        
-                            grid[i, y1] = 1;
-                      
-
+                        grid[i, y1] = 1;
                     }
-                    
-
-                        lblGrid.Text = "";
-                        GetGrid(grid);
-                        accu++;
-
-                    
+                    lblGrid.Text = "";
+                    GetGrid(grid);
+                    accu++;
                 }
                 if(xdif == 2 && accu == 2 || accu == 3 || accu == 4)
                 {
@@ -149,16 +163,74 @@ namespace Battleship
                         ResetGrid();
                     }
                 }
-
-
             }
-
-            if(x1 == x2) //If you are staying in the row
+            if(x1 == x2) // If you are staying in the row
             {
+                if (ydif == 3 && accu == 1)
+                {
+                    for (int i = y1; i <= y2; i++)
+                    {
+                        grid[x1, i] = 1;
+                    }
+                    lblGrid.Text = "";
+                    GetGrid(grid);
+                    accu++;
+                }
+                if (ydif == 2 && accu == 2 || accu == 3 || accu == 4)
+                {
+                    for (int i = y1; i <= y2; i++)
+                    {
+                        if (grid[x1, i] == 0)
+                        {
+                            grid[x1, i] = 1;
+                        }
+                        else
+                        {
+                            n1--;
+                        }
+                    }
+                    if (n1 == 0)
+                    {
 
+                        lblGrid.Text = "";
+                        GetGrid(grid);
+                        accu++;
+                        ns--;
+                    }
+                    else
+                    {
+                        ResetGrid();
+                    }
+                }
+                if (ydif == 1 && accu == 5 || accu == 6 || accu == 7 || accu == 8)
+                {
+                    for (int i = y1; i <= y2; i++)
+                    {
+                        if (grid[x1, i] == 0)
+                        {
+                            grid[x1, i] = 1;
+                        }
+                        else
+                        {
+                            n1--;
+                        }
+                    }
+                    if (n1 == 0)
+                    {
+
+                        lblGrid.Text = "";
+                        GetGrid(grid);
+                        accu++;
+                        ns2--;
+                    }
+                    else
+                    {
+                        ResetGrid();
+                    }
+                }
             }
             
-            if(x1 != x2 && y1 != y2)//If they try to go diagonally 
+            if(x1 != x2 && y1 != y2)// If they try to go diagonally 
             {
                 MessageBox.Show("Ships can't go diagonally.");
             }
@@ -166,7 +238,7 @@ namespace Battleship
         }
         private void Form1_Load(object sender, EventArgs e)
         {
-            
+            GetAttackGrid(attackgrid);
             GetGrid(grid);
         }
 
@@ -180,17 +252,14 @@ namespace Battleship
                 for (int i = 0; i < txtXY.Text.Length; i++)
                 {
                     
-                        if (input[i] == '0' || input[i] == '1' || input[i] == '2' || input[i] == '3' ||
-                            input[i] == '4' || input[i] == '5' || input[i] == '6' || input[i] == '7')
-                        {
-                            shippos[a2] = (int)char.GetNumericValue(input[i]);
-                            a2++;
-                        }
-                        
+                    if (input[i] == '0' || input[i] == '1' || input[i] == '2' || input[i] == '3' ||
+                        input[i] == '4' || input[i] == '5' || input[i] == '6' || input[i] == '7')
+                    {
+                        shippos[a2] = (int)char.GetNumericValue(input[i]);
+                        a2++;
+                    }  
                 }
                 SetShip();
-                
-                
                 if(accu == 2)
                 {
                     lblShips.Text = "Destroyer - " + ns.ToString() + " Left";
@@ -202,14 +271,11 @@ namespace Battleship
                 }
                 else if(accu == 5)
                 {
-                    
                     lblShips.Text = "Submarine - " + ns2.ToString() + " Left";
-                    
                 }
                 else if(accu == 6 || accu == 7 || accu == 8)
                 {
                     lblShips.Text = "Submarine - " + ns2.ToString() + " Left";
-                    
                 }
                 else
                 {
