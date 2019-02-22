14a76b5dc4f1de2948d45db47a38bee3c2c4009b
R4F42 <45702449+R4F42@users.noreply.github.com>
2019-02-21 21:22:19 -0600

    Added project file.

---
+++ Battleship/Battleship/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Battleship
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblGrid = new System.Windows.Forms.Label();
+            this.txtXY = new System.Windows.Forms.TextBox();
+            this.btnSet = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAttack = new System.Windows.Forms.Button();
+            this.txtAttack = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblShips = new System.Windows.Forms.Label();
+            this.lblAttack = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblGrid
+            // 
+            this.lblGrid.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblGrid.Location = new System.Drawing.Point(57, 41);
+            this.lblGrid.Name = "lblGrid";
+            this.lblGrid.Size = new System.Drawing.Size(247, 257);
+            this.lblGrid.TabIndex = 0;
+            this.lblGrid.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // txtXY
+            // 
+            this.txtXY.Location = new System.Drawing.Point(57, 348);
+            this.txtXY.Name = "txtXY";
+            this.txtXY.Size = new System.Drawing.Size(100, 20);
+            this.txtXY.TabIndex = 1;
+            // 
+            // btnSet
+            // 
+            this.btnSet.Location = new System.Drawing.Point(163, 348);
+            this.btnSet.Name = "btnSet";
+            this.btnSet.Size = new System.Drawing.Size(75, 23);
+            this.btnSet.TabIndex = 2;
+            this.btnSet.Text = "Set Ships";
+            this.btnSet.UseVisualStyleBackColor = true;
+            this.btnSet.Click += new System.EventHandler(this.btnSet_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 313);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Format : x1 y1 x2 y2 or x1y1x2y2";
+            // 
+            // btnAttack
+            // 
+            this.btnAttack.Location = new System.Drawing.Point(163, 389);
+            this.btnAttack.Name = "btnAttack";
+            this.btnAttack.Size = new System.Drawing.Size(75, 23);
+            this.btnAttack.TabIndex = 4;
+            this.btnAttack.Text = "Attack";
+            this.btnAttack.UseVisualStyleBackColor = true;
+            this.btnAttack.Click += new System.EventHandler(this.btnAttack_Click);
+            // 
+            // txtAttack
+            // 
+            this.txtAttack.Location = new System.Drawing.Point(57, 392);
+            this.txtAttack.Name = "txtAttack";
+            this.txtAttack.Size = new System.Drawing.Size(100, 20);
+            this.txtAttack.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(56, 376);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Format : x1 y1";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(476, 332);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 80);
+            this.btnReset.TabIndex = 7;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // lblShips
+            // 
+            this.lblShips.AutoSize = true;
+            this.lblShips.Location = new System.Drawing.Point(56, 332);
+            this.lblShips.Name = "lblShips";
+            this.lblShips.Size = new System.Drawing.Size(89, 13);
+            this.lblShips.TabIndex = 8;
+            this.lblShips.Text = "Battleship - 1 Left";
+            // 
+            // lblAttack
+            // 
+            this.lblAttack.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblAttack.Location = new System.Drawing.Point(321, 42);
+            this.lblAttack.Name = "lblAttack";
+            this.lblAttack.Size = new System.Drawing.Size(227, 256);
+            this.lblAttack.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Location = new System.Drawing.Point(117, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(142, 23);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Ship Logistics";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Location = new System.Drawing.Point(363, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(142, 23);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Enemy Region";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(636, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblAttack);
+            this.Controls.Add(this.lblShips);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtAttack);
+            this.Controls.Add(this.btnAttack);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSet);
+            this.Controls.Add(this.txtXY);
+            this.Controls.Add(this.lblGrid);
+            this.Name = "Form1";
+            this.Opacity = 0.9D;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblGrid;
+        private System.Windows.Forms.TextBox txtXY;
+        private System.Windows.Forms.Button btnSet;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAttack;
+        private System.Windows.Forms.TextBox txtAttack;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblShips;
+        private System.Windows.Forms.Label lblAttack;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Battleship/Battleship/Form1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Battleship
+{
+    public partial class Form1 : Form
+    {
+        public int[,] grid = { {0, 0, 0, 0, 0, 0, 0, 0},
+                               {0, 0, 0, 0, 0, 0, 0, 0},
+                               {0, 0, 0, 0, 0, 0, 0, 0},
+                               {0, 0, 0, 0, 0, 0, 0, 0},
+                               {0, 0, 0, 0, 0, 0, 0, 0},
+                               {0, 0, 0, 0, 0, 0, 0, 0},
+                               {0, 0, 0, 0, 0, 0, 0, 0},
+                               {0, 0, 0, 0, 0, 0, 0, 0}, };
+        
+
+        public int[] shippos = new int[4];
+        public int accu = 1;
+        public int ns = 3,ns2 = 4;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void GetGrid(int[,] gridname)
+        {
+            for(int i = 0; i < 8; i++)
+            {
+                for(int j = 0; j < 8; j++)
+                {
+                    lblGrid.Text += gridname[i, j].ToString() + "      ";
+                    if (j == 7)
+                    {
+                        lblGrid.Text += "\r\n\r\n";
+                    }
+                }
+            }
+        }
+        
+        private void ResetGrid()
+        {
+            int[,] grid =     {{ 0, 0, 0, 0, 0, 0, 0, 0},
+                               { 0, 0, 0, 0, 0, 0, 0, 0},
+                               { 0, 0, 0, 0, 0, 0, 0, 0},
+                               { 0, 0, 0, 0, 0, 0, 0, 0},
+                               { 0, 0, 0, 0, 0, 0, 0, 0},
+                               { 0, 0, 0, 0, 0, 0, 0, 0},
+                               { 0, 0, 0, 0, 0, 0, 0, 0},
+                               { 0, 0, 0, 0, 0, 0, 0, 0}, };
+        }
+        private void SetShip()
+        {
+            int x1 = shippos[0], y1 = shippos[1], x2 = shippos[2], y2 = shippos[3];
+            int xdif = 0, ydif = 0;
+
+            if(x1 - x2 < 0) //Find diff between x's
+            {
+                xdif = (x1 - x2) * -1;
+            }
+            else
+            {
+                xdif = x1 - x2;
+            }
+
+            if (y1 - y2 < 0) //Find diff between y's
+            {
+                ydif = (y1 - y2) * -1;
+            }
+            else
+            {
+                ydif = y1 - y2;
+            }
+            int n1 = 0;
+            if (y1 == y2) //If you are staying in the column
+            {
+                if (xdif == 3 && accu == 1)
+                {
+                    for(int i = x1; i <= x2; i++)
+                    {
+                        
+                            grid[i, y1] = 1;
+                      
+
+                    }
+                    
+
+                        lblGrid.Text = "";
+                        GetGrid(grid);
+                        accu++;
+
+                    
+                }
+                if(xdif == 2 && accu == 2 || accu == 3 || accu == 4)
+                {
+                    for (int i = x1; i <= x2; i++)
+                    {
+                        if (grid[i, y1] == 0)
+                        {
+                            grid[i, y1] = 1;
+                        }
+                        else
+                        {
+                            n1--;
+                        }
+                    }
+                    if (n1 == 0)
+                    {
+                        
+                        lblGrid.Text = "";
+                        GetGrid(grid);
+                        accu++;
+                        ns--;
+                    }
+                    else
+                    {
+                        ResetGrid();
+                    }
+                }
+                if (xdif == 1 && accu == 5 || accu == 6 || accu == 7 || accu == 8)
+                {
+                    for (int i = x1; i <= x2; i++)
+                    {
+                        if (grid[i, y1] == 0)
+                        {
+                            grid[i, y1] = 1;
+                        }
+                        else
+                        {
+                            n1--;
+                        }
+                    }
+                    if (n1 == 0)
+                    {
+
+                        lblGrid.Text = "";
+                        GetGrid(grid);
+                        accu++;
+                        ns2--;
+                    }
+                    else
+                    {
+                        ResetGrid();
+                    }
+                }
+
+
+            }
+
+            if(x1 == x2) //If you are staying in the row
+            {
+
+            }
+            
+            if(x1 != x2 && y1 != y2)//If they try to go diagonally 
+            {
+                MessageBox.Show("Ships can't go diagonally.");
+            }
+            txtXY.Text = "";
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            GetGrid(grid);
+        }
+
+        private void btnSet_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                char[] input;
+                input = txtXY.Text.ToArray();
+                int a2 = 0;
+                for (int i = 0; i < txtXY.Text.Length; i++)
+                {
+                    
+                        if (input[i] == '0' || input[i] == '1' || input[i] == '2' || input[i] == '3' ||
+                            input[i] == '4' || input[i] == '5' || input[i] == '6' || input[i] == '7')
+                        {
+                            shippos[a2] = (int)char.GetNumericValue(input[i]);
+                            a2++;
+                        }
+                        
+                }
+                SetShip();
+                
+                
+                if(accu == 2)
+                {
+                    lblShips.Text = "Destroyer - " + ns.ToString() + " Left";
+                }
+                else if (accu == 3 || accu == 4)
+                {
+                    lblShips.Text = "Destroyer - " + ns.ToString() + " Left";
+                    
+                }
+                else if(accu == 5)
+                {
+                    
+                    lblShips.Text = "Submarine - " + ns2.ToString() + " Left";
+                    
+                }
+                else if(accu == 6 || accu == 7 || accu == 8)
+                {
+                    lblShips.Text = "Submarine - " + ns2.ToString() + " Left";
+                    
+                }
+                else
+                {
+                    lblShips.Text = "You are out of ships.";
+                }
+            }
+            catch(Exception ex)
+            {
+               MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btnAttack_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            ns = 3;
+            ns2 = 4;
+            accu = 1;
+            lblShips.Text = "Battleship - 1 left";
+            lblGrid.Text = "";
+            int[,] grid =     {{ 0, 0, 0, 0, 0, 0, 0, 0},
+                               { 0, 0, 0, 0, 0, 0, 0, 0},
+                               { 0, 0, 0, 0, 0, 0, 0, 0},
+                               { 0, 0, 0, 0, 0, 0, 0, 0},
+                               { 0, 0, 0, 0, 0, 0, 0, 0},
+                               { 0, 0, 0, 0, 0, 0, 0, 0},
+                               { 0, 0, 0, 0, 0, 0, 0, 0},
+                               { 0, 0, 0, 0, 0, 0, 0, 0}, };
+            GetGrid(grid);
+            txtXY.Text = "";
+        }
+    }
+}
